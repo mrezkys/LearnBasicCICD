3b96c8780047f2be8ce4f5de8a0684abd31cf173
mrezkys <mrezkysulihin@gmail.com>
2025-09-03 16:48:39 +0800

    Initial Commit

---
+++ LearnBasicCICD/LearnBasicCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnBasicCICDApp.swift
+//  LearnBasicCICD
+//
+//  Created by Muhammad Rezky on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnBasicCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnBasicCICDTests/LearnBasicCICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnBasicCICDTests.swift
+//  LearnBasicCICDTests
+//
+//  Created by Muhammad Rezky on 03/09/25.
+//
+
+import Testing
+@testable import LearnBasicCICD
+
+struct LearnBasicCICDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
